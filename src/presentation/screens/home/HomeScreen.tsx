c09a10ac8c2e0c9a7e4598921f2bbe5b55e6d7fa
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-21 13:41:53 -0500

    103. Navegar a otras pantallas

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { globalStyles } from '../../theme/theme';
+import { useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
 
 export const HomeScreen = () => {
+	const navigation = useNavigation();
+
 	return (
-		<View>
-			<Text>HomeScreen</Text>
+		<View style={globalStyles.container}>
+			<PrimaryButton
+				onPress={() => navigation.navigate('Products' as never)}
+				label="Productos"
+			/>
+
+			<PrimaryButton
+				onPress={() => navigation.navigate('Settings' as never)}
+				label="Settings"
+			/>
 		</View>
 	);
 };
