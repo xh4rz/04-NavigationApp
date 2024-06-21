5c341a72704479922bf0de9547438cbee923b870
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-21 13:19:48 -0500

    102. React Navigation - Stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text } from 'react-native';
+import { StackNavigator } from './presentation/routes/StackNavigator';
 
 export const App = () => {
 	return (
 		<NavigationContainer>
-			<Text>App.tsx</Text>
+			<StackNavigator />
 		</NavigationContainer>
 	);
 };
