1218e0b1cc86c327cb63e5958b001d205e94236c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-21 13:45:45 -0500

    104. Estilizando el Stack Navigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -7,7 +7,14 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
 	return (
-		<Stack.Navigator>
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: true,
+				headerStyle: {
+					elevation: 0,
+					shadowColor: 'transparent'
+				}
+			}}>
 			<Stack.Screen name="Home" component={HomeScreen} />
 			<Stack.Screen name="Products" component={ProductsScreen} />
 			<Stack.Screen name="Settings" component={SettingsScreen} />
