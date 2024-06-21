547b7be04c3bfd7359d7da2e8141a83a8c5a888e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-21 14:00:12 -0500

    105. FlatList - Pantalla de productos

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/home/HomeScreen';
 import { ProductsScreen } from '../screens/products/ProductsScreen';
+import { ProductScreen } from '../screens/products/ProductScreen';
 import { SettingsScreen } from '../screens/settings/SettingsScreen';
 
 const Stack = createStackNavigator();
@@ -17,6 +18,7 @@ export const StackNavigator = () => {
 			}}>
 			<Stack.Screen name="Home" component={HomeScreen} />
 			<Stack.Screen name="Products" component={ProductsScreen} />
+			<Stack.Screen name="Product" component={ProductScreen} />
 			<Stack.Screen name="Settings" component={SettingsScreen} />
 		</Stack.Navigator>
 	);
