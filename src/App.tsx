0f46bced69f940474c3a5a060bb3900815098d68
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-24 11:43:14 -0500

    109. Configurar Drawer básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/routes/StackNavigator';
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
 	return (
 		<NavigationContainer>
-			<StackNavigator />
+			{/* <StackNavigator /> */}
+			<SideMenuNavigator />
 		</NavigationContainer>
 	);
 };
